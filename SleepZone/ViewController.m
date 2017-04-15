ef194cf0e0033ee2f79319e6fc6a040ef7f56b95
Aaron Schachter <aaronschachter@gmail.com>
2017-04-15 11:41:56 -0700

    Adds UITapGestureRecognizers to control players

--- SleepZone/ViewController.m
+++ SleepZone/ViewController.m
@@ -12,9 +12,13 @@
 
 @interface ViewController ()
 
+@property (assign, nonatomic) BOOL isPlayingRain;
+@property (assign, nonatomic) BOOL isPlayingShusher;
 @property (strong, nonatomic) AVAudioPlayer *rainPlayer;
 @property (strong, nonatomic) AVAudioPlayer *shusherPlayer;
 @property (weak, nonatomic) IBOutlet UISwitch *shusherSwitch;
+@property (weak, nonatomic) IBOutlet UIView *shusherContainerView;
+@property (weak, nonatomic) IBOutlet UIView *rainContainerView;
 
 @property (weak, nonatomic) IBOutlet UISwitch *rainSwitch;
 - (IBAction)rainSwitchValueChanged:(id)sender;
@@ -36,28 +40,51 @@
     self.shusherPlayer.numberOfLoops = -1;
 
     [self.shusherSwitch setOn:NO];
+    self.isPlayingShusher = NO;
 
     NSString *rainFilePath = [NSString stringWithFormat:@"%@/rain.wav",[[NSBundle mainBundle] resourcePath]];
     NSURL *rainFileURL = [NSURL fileURLWithPath:rainFilePath];
     self.rainPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:rainFileURL error:nil];
     self.rainPlayer.numberOfLoops = -1;
     [self.rainPlayer setVolume:0.25];
+
     [self.rainSwitch setOn:NO];
+    self.isPlayingRain = NO;
+    
+     UITapGestureRecognizer *shusherTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleShusherTap:)];
+    [self.shusherContainerView addGestureRecognizer:shusherTap];
+    UITapGestureRecognizer *rainTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleRainTap:)];
+    [self.rainContainerView addGestureRecognizer:rainTap];
 }
 
-- (IBAction)shusherSwitchValueChanged:(id)sender {
-    if (self.shusherSwitch.isOn) {
-        [self.shusherPlayer play];
-    } else {
-        [self.shusherPlayer pause];
-    }
+- (void)handleRainTap:(UITapGestureRecognizer *)recognizer {
+    [self togglePlayingRain];
 }
 
-- (IBAction)rainSwitchValueChanged:(id)sender {
-    if (self.rainSwitch.isOn) {
+- (void)handleShusherTap:(UITapGestureRecognizer *)recognizer {
+    [self togglePlayingShusher];
+}
+
+- (void)togglePlayingRain {
+    self.isPlayingRain = !self.isPlayingRain;
+
+    if (self.isPlayingRain) {
         [self.rainPlayer play];
     } else {
         [self.rainPlayer pause];
     }
+    [self.rainSwitch setOn:self.isPlayingRain animated:YES];
 }
+
+- (void)togglePlayingShusher {
+    self.isPlayingShusher = !self.isPlayingShusher;
+
+    if (self.isPlayingShusher) {
+        [self.shusherPlayer play];
+    } else {
+        [self.shusherPlayer pause];
+    }
+    [self.shusherSwitch setOn:self.isPlayingShusher animated:YES];
+}
+
 @end
